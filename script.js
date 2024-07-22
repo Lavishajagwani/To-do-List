9c9a4bcceeb01b72857b216bd3ec74c6814fd652
Lavisha <lavishajagwani@gmail.com>
2024-07-22 15:59:01 +0530

    final changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Select the elements
-const input = document.querySelector("#input");
+const input = document.querySelector(".input");
 const addbtn = document.querySelector(".addbtn");
-const taskList = document.querySelector("#list");
-const delbtn = document.querySelector(".delbtn");
+const taskList = document.querySelector(".list");
+const delAllbtn = document.querySelector(".delAllbtn");
 
 addbtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -13,19 +13,42 @@ function addTask() {
   const task = input.value.trim();
   if (task) {
     const listItem = document.createElement("li");
+
     const checkBox = document.createElement("input");
-    checkBox.type = "checkBox";
+    checkBox.type = "checkbox";
+    checkBox.addEventListener("click", toggleTaskCompleted);
+
     const label = document.createElement("label");
     label.textContent = task;
+
+    const delBtn = document.createElement("button");
+    delBtn.className = "delBtn";
+    const icon = document.createElement("i");
+    icon.className = "fa-solid fa-minus"
+
+    delBtn.addEventListener("click", deleteTask);
+    delBtn.appendChild(icon);
     listItem.appendChild(checkBox);
     listItem.appendChild(label);
+    listItem.appendChild(delBtn);
     taskList.appendChild(listItem);
 
     input.value = "";
   }
 }
 
-delbtn.addEventListener("click", deleteAllTasks);
+function toggleTaskCompleted(event) {
+  const listItem = event.target.parentNode;
+  listItem.classList.toggle("completed");
+}
+
+function deleteTask(event) {
+  const delBtn = event.target.closest('button');
+  const listItem = delBtn.parentNode;
+  listItem.remove();
+}
+
+delAllbtn.addEventListener("click", deleteAllTasks);
 
 function deleteAllTasks() {
     taskList.innerHTML = "";
